8a0f90b6afc6755310e7f42e7b280f08b5aeaee8
Beforerr <zzj956959688@gmail.com>
2025-04-18 15:40:22 -0700

    ci: benchmark

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,3 +5,4 @@ const SUITE = BenchmarkGroup()
 
 p = RDProblemParams(θ=45, β=90, v=8)
 SUITE["main"]["RotationalDiscontinuity"] = @benchmarkable solve_params($p)
+SUITE["main"]["RotationalDiscontinuity - No save"] = @benchmarkable solve_params($p, save_everystep=false)
