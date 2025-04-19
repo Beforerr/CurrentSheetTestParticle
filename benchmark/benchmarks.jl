5472b05831d1d24e2c172bf4d36801b741c0381a
Beforerr <zzj956959688@gmail.com>
2025-04-18 17:39:12 -0700

    CI: fix @benchmarkable

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -7,9 +7,8 @@ const SUITE = BenchmarkGroup()
 p = RDProblemParams(θ=45, β=90, v=8)
 SUITE["main"]["RotationalDiscontinuity"] = @benchmarkable solve_params($p)
 SUITE["main"]["RotationalDiscontinuity - No save"] = @benchmarkable solve_params($p, save_everystep=false)
-
-SUITE["main"]["RotationalDiscontinuity - No save - NullLogger"] = with_logger(NullLogger()) do
-    @benchmarkable solve_params($p, save_everystep=false)
+SUITE["main"]["RotationalDiscontinuity - No save - NullLogger"] = @benchmarkable with_logger(NullLogger()) do
+    solve_params($p, save_everystep=false)
 end
 
 @static if Sys.isapple()
