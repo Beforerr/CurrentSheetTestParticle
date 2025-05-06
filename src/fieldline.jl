0e80594b1e554196739462d384c87ff7b5ab65d3
Beforerr <zzj956959688@gmail.com>
2025-05-05 17:12:13 -0700

    feat: faster field_lines_asym_distance

--- src/fieldline.jl
+++ src/fieldline.jl
@@ -54,3 +54,19 @@ end
 for sym in [:field_lines_distance, :field_lines_asym_distance]
     @eval $sym(sol, B) = $sym(field_lines(sol, B)..., B)
 end
+
+# Accelerated version of `field_lines_asym_distance`
+function _field_lines_asym_distance(sol, B)
+    direction = B([0, 0, Inf])
+    gc0 = guiding_center(sol[1], B)
+    gcf = guiding_center(sol[end], B)
+    if sign(gc0[3]) == sign(gcf[3])
+        return distance(gc0, gcf, direction)
+    else
+        isoutofdomain = (u, p, t) -> abs(u[3]) > maximum(abs.(sol[3, :]))
+        Bz0 = B(gc0)[3] # this only works for constant Bz
+        tmax = 100 * sol.t[end]
+        flf_sol = solve_fl(gcf, B; tspan=(0, -sign(Bz0 * gcf[3]) * tmax), isoutofdomain, save_everystep=false)
+        return distance(gc0, flf_sol[end], direction)
+    end
+end
